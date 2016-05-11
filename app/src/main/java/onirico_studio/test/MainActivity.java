4e4e1776c0856a2171d05a29af643a489366baed
Levi <lothar.one@outlook.com>
2016-05-11 16:07:48 -0500

    Initial commit

---
+++ app/src/main/java/onirico_studio/test/MainActivity.java
@@ -0,0 +1,13 @@
+package onirico_studio.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
